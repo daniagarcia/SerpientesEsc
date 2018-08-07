55e168acd80cae4f30ec61b976252ca9b59fdf32
daniagarcia <daniag.serguridad@gmail.com>
2018-08-07 15:57:58 -0500

    metodo iniciar

---
+++ app/Controllers/Ws/JuegoController.js
@@ -0,0 +1,26 @@
+'use strict'
+
+class JuegoController {
+  constructor ({ socket, request }) {
+    this.socket = socket
+    this.request = request
+  }
+      onMessage(message){
+        this.socket.broadcast("message",message);
+    }
+
+    onOpen(message){
+        this.socket.broadcast("message",message);
+    }
+
+    onClose(message){
+        this.socket.broadcast("message",message);
+    }
+    
+    onPartida(){
+
+    }
+
+}
+
+module.exports = JuegoController
